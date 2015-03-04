4f6f6532f41dfc00fdca1a73e85f1776838a0a42
CJ <cjting1128@gmail.com>
2015-03-04 17:05:04 +0800

    day 2,opening a Win32 window

--- code/win32_handmade.cpp
+++ code/win32_handmade.cpp
@@ -1,12 +1,117 @@
 #include <windows.h>
 
-int CALLBACK
-WinMain(HINSTANCE hInstance,
-	HINSTANCE hPrevInstance,
-	LPSTR lpCmdLine,
-	int nCmdShow)
+LRESULT CALLBACK MainWindowCallback(HWND Window,
+                            UINT Message,
+                            WPARAM wParam,
+                            LPARAM lParam)
 {
-  MessageBox(0,"this is a handmade  hreo","handmade",MB_OK | MB_ICONINFORMATION );
+  LRESULT Result = 0;
+  switch(Message){
+    case WM_SIZE:
+    {
+      OutputDebugStringA("SIZE\n");
+    } break;
+
+    case WM_DESTROY:
+    {
+      OutputDebugStringA("DESTROY\n");
+    } break;
+
+    case WM_CLOSE:
+    {
+      OutputDebugStringA("CLOSE\n");
+    } break;
+
+    case WM_ACTIVATEAPP:
+    {
+      OutputDebugStringA("ACTIVATEAPP\n");
+    } break;
+
+    case WM_PAINT:
+    {
+      PAINTSTRUCT Paint;
+      HDC DeviceContext = BeginPaint(Window,&Paint);
+      int Width = Paint.rcPaint.right - Paint.rcPaint.left;
+      int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
+      static DWORD Color = WHITENESS;
+      PatBlt(
+        DeviceContext,
+        Paint.rcPaint.left,
+        Paint.rcPaint.top,
+        Width,
+        Height,
+        Color
+      );
+      if(Color == WHITENESS){
+        Color = BLACKNESS;
+      }
+      else{
+        Color = WHITENESS;
+      }
+      EndPaint(Window,&Paint);
+    } break;
+
+    default:
+    {
+      OutputDebugStringA("DEFAULT\n");
+      Result = DefWindowProc(Window,Message,wParam,lParam);
+    }
+  }
+
+  return Result;
+}
+
+int CALLBACK
+WinMain(HINSTANCE Instance,
+	HINSTANCE PrevInstance,
+	LPSTR CmdLine,
+	int CmdShow)
+{
+  WNDCLASS WindowClass = {};
+
+  //TODO: check if owndc,hredraw,vredraw matter
+  WindowClass.style = CS_OWNDC|CS_HREDRAW|CS_VREDRAW;
+  WindowClass.lpfnWndProc = MainWindowCallback;
+  WindowClass.hInstance = Instance;
+  // HICON     hIcon;
+  WindowClass.lpszClassName = "HandmadeHeroWindowClass";
+
+  if(RegisterClass(&WindowClass))
+  {
+    HWND WindowHandle =  CreateWindowEx(
+      0,
+      WindowClass.lpszClassName,
+      "HandmadeHero",
+      WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+      CW_USEDEFAULT,
+      CW_USEDEFAULT,
+      CW_USEDEFAULT,
+      CW_USEDEFAULT,
+      0,
+      0,
+      Instance,
+      0
+    );
+    if(WindowHandle){
+      MSG Message;
+      for(;;){
+        if(GetMessage(&Message,WindowHandle,0,0) > 0){
+          TranslateMessage(&Message);
+          DispatchMessage(&Message);
+        }
+        else{
+          break;
+        }
+      }
+    }
+    else{
+      //TODO logging
+    }
+  }
+  else{
+    //TODO logging
+  }
+
   return 0;
 }
 
--- misc/build.bat
+++ misc/build.bat
@@ -1,6 +1,6 @@
 @echo off
 mkdir ..\..\build
 pushd ..\..\build
-cl /Zi ..\handmade\code\win32_handmade.cpp user32.lib
+cl /Zi ..\handmade\code\win32_handmade.cpp user32.lib gdi32.lib
 popd
 
