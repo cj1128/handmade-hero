eb1ca6337f950486bdc90e94b6ba4dee33153602
CJ <cjting1128@gmail.com>
2015-03-04 14:52:37 +0800

    day 1,setting up windows dev environment

---
+++ code/win32_handmade.cpp
@@ -0,0 +1,12 @@
+#include <windows.h>
+
+int CALLBACK
+WinMain(HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine,
+	int nCmdShow)
+{
+  MessageBox(0,"this is a handmade  hreo","handmade",MB_OK | MB_ICONINFORMATION );
+  return 0;
+}
+
---
+++ misc/shell.bat
@@ -0,0 +1,4 @@
+@echo off
+call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x64
+set path=v:\handmade\misc;%path%
+v:
