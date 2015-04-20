8578cb48c3aea64b3adcc7a202436fbba92d2c45
CJ <cjting1128@gmail.com>
2015-04-20 16:00:16 +0800

    Setup Sublime Build System to build the code

--- misc/build.bat
+++ misc/build.bat
@@ -1,6 +1,7 @@
 @echo off
 mkdir ..\..\build
 pushd ..\..\build
+call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x64
 cl /Zi ..\handmade\code\win32_handmade.cpp user32.lib gdi32.lib
 popd
 
